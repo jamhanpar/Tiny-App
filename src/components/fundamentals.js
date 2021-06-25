fd88d74f9e013d176ce20fad6ec134cc9070184b
James <james.hansung.park@gmail.com>
2021-06-25 19:01:35 -0400

    updated code to use custom hooks

--- src/components/fundamentals.js
+++ src/components/fundamentals.js
@@ -2,42 +2,68 @@ import React, { useState, useEffect } from 'react';
 import Row from "react-bootstrap/Row";
 
 export default function Greeting(props) {
-    const [name, setName] = useState('James');
-    const [surname, setSurname] = useState('Park');
+    const name = useFormInput('James');
+    const surname = useFormInput('Park');
+    // const [name, setName] = useState('James');
+    // const [surname, setSurname] = useState('Park');
+    const width = useWindowWidth();
+    // useDocumentTitle(name + ' ' + surname);
+    useDocumentTitle(name.value + ' ' + surname.value);
 
-    useEffect(() => {
-        document.title = name + ' ' + surname;
-    });
+    // function handleNameChange(e) {
+    //     setName(e.target.value);
+    // }
 
-    const [width, setWidth] = useState(window.innerWidth);
-
-    useEffect(() => {
-        const handleResize = () => setWidth(window.innerWidth);
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize)
-        }
-    });
-
-    function handleNameChange(e) {
-        setName(e.target.value);
-    }
-
-    function handleSurnameChange(e) {
-        setSurname(e.target.value);
-    }
+    // function handleSurnameChange(e) {
+    //     setSurname(e.target.value);
+    // }
 
     return (
       <section className="widget">
         <div className="info" label="Name">
-          <input value={name} onChange={handleNameChange} />
+          {/* <input value={name} onChange={handleNameChange} /> */}
+          <input {...name} />
         </div>
         <div className="info" label="Surname">
-          <input value={surname} onChange={handleSurnameChange} />
+          {/* <input value={surname} onChange={handleSurnameChange} /> */}
+          <input {...surname} />
         </div>
         <div className="info" label="Width">
-          {width}
+          {`width: ${width}`}
         </div>
       </section>
     );
+}
+
+function useFormInput(initialValue) {
+    const [value, setValue] = useState(initialValue);
+
+    function handleChange(e) {
+      setValue(e.target.value);
+    }
+    
+    return {
+        value,
+        onChange: handleChange
+    }
+}
+
+function useDocumentTitle(title) {
+    useEffect(() => {
+        document.title = title;
+    });
+}
+
+function useWindowWidth() {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => setWidth(window.innerWidth);
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    });
+    
+    return width;
 }
