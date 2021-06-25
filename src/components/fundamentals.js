4b295da58c8cc10e6cfa1caf830ff6163fa9c15b
James <james.hansung.park@gmail.com>
2021-06-25 17:32:01 -0400

    created width variable and useEffect to update as window adjusts

--- src/components/fundamentals.js
+++ src/components/fundamentals.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Row from "react-bootstrap/Row";
 
 export default function Greeting(props) {
     const [name, setName] = useState('James');
     const [surname, setSurname] = useState('Park');
+
+    useEffect(() => {
+        document.title = name + ' ' + surname;
+    });
+
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+    });
 
     function handleNameChange(e) {
         setName(e.target.value);
@@ -14,19 +25,16 @@ export default function Greeting(props) {
     }
 
     return (
-        <section className="widget">
-            <Row label="Name">
-                <input
-                    value={name}
-                    onChange={handleNameChange}
-                    />
-            </Row>
-            <Row label="Surname">
-                <input
-                    value={surname}
-                    onChange={handleSurnameChange}
-                    />
-            </Row>
-        </section>
-    )
+      <section className="widget">
+        <div className="info" label="Name">
+          <input value={name} onChange={handleNameChange} />
+        </div>
+        <div className="info" label="Surname">
+          <input value={surname} onChange={handleSurnameChange} />
+        </div>
+        <div className="info" label="Width">
+          {width}
+        </div>
+      </section>
+    );
 }
