95740cbcdfb062054845e0a721eb77a0a673956e
James <james.hansung.park@gmail.com>
2021-06-25 21:04:45 -0400

    added reducer using state and dispatch

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,23 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import Button from '@material-ui/core/Button';
 
 export default function Carousel() {
     let [currentIdx, setCurrentIdx] = useState(0);
     let [isPlaying, setIsPlaying] = useState(false);
+    let [state, dispatch] = useReducer((state, action) => {
+        switch(action.type) {
+            case 'PROGRESS':
+            case 'NEXT': return {
+                ...state,
+                isPlaying: action.type === 'PROGRESS',
+                currentIdx: (currentIdx + 1) % slides.length
+            }
+            case 'PREV': return {
+                ...state,
+                isPlaying: false,
+                currentIdx: (currentIdx - 1) % slides.length
+            }
+            case 'PLAY': return {
+                ...state,
+                isPlaying: true
+            }
+            case 'PAUSE': return {
+                ...state,
+                isPlaying: false
+            }
+            default: return state
+        }
+    }, {
+        currentIndex: 0,
+        isPlaying: false
+    })
     let slides = [];
     let SLIDE_DURATION = 2000;
 
     useEffect(() => {
-        if (isPlaying) {
+        // if (isPlaying) {
+        if (state.isPlaying) {
             let timeout = setTimeout(() => {
-                setCurrentIdx(
-                    (currentIdx + 1) % slides.length
-                );
+                dispatch({ type: 'PROGRESS' })
+                // setCurrentIdx(
+                //     (currentIdx + 1) % slides.length
+                // );
             }, SLIDE_DURATION);
             
             return () => clearTimeout(timeout);
         };
-    }, [currentIdx, isPlaying]);
+    // }, [currentIdx, isPlaying]);
+    }, [state.currentIdx, state.isPlaying]);
 
     return (
         <section className="widget">
